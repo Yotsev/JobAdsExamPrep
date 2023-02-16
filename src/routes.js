6430747e1a0ddddf3cf032610724cf8906d262e1
Yotsev <b.yotsev@gmail.com>
2023-02-16 15:23:58 +0200

    Added project resourses

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,9 @@ const router = require('express').Router();
 const { authentication } = require('./middlewares/authMiddleware');
 
 const authController = require('./controllers/authController');
-const homeController = require('./controllers/homeController');
-const actionController = require('./controllers/actionController');
 
-router.use('/', homeController);
 router.use('/auth', authController);
-router.use('/action', actionController);
+
 // router.get('*', (req, res)=> {
 //     res.render('home/404');
 // });
